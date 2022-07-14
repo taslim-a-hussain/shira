95379f7a02165193a2b2728ae5a66b88e4340b3c
Taslim A Hussain <taslim@outlook.co.nz>
2022-07-14 13:33:01 +1200

    Dir structure update

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '../components/button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
