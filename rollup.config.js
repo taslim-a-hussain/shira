d7a897b379d8de5bb501620d3194f3a176178016
Taslim A Hussain <taslim@outlook.co.nz>
2022-07-14 16:18:38 +1200

    Patch 1

--- rollup.config.js
+++ rollup.config.js
@@ -5,23 +5,39 @@ import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import {uglify} from 'rollup-plugin-uglify';
 import {babel} from '@rollup/plugin-babel';
+import dts from 'rollup-plugin-dts';
 
-export default {
-    input: "./stories/index.ts",
-    output: [
-        {
-            file: "lib/index.js",
-            format: "es"
-        }
-    ],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        typescript(),
-        postcss({
-            extensions: ['.css']
-        }),
-        uglify(),
-        babel()
-    ]
-}
+export default [
+    {
+        input: "./src/index.ts",
+        output: [
+            {
+                file: "lib/index.cjs.js",
+                format: "cjs"
+            },
+            {
+                file: "lib/index.esm.js",
+                format: "es"
+            }
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            typescript({
+                tsconfig: 'tsconfig.json',
+                useTsconfigDeclarationDir: true
+            }),
+            postcss({
+                extensions: ['.css']
+            }),
+            uglify(),
+            babel({ babelHelpers: 'bundled' })
+        ]
+    },
+    {
+        input: 'lib/types/index.d.ts',
+        output: [{ file: 'lib/index.d.ts', format: "esm" }],
+        external: [/\.css$/],
+        plugins: [dts()]
+    }
+]
